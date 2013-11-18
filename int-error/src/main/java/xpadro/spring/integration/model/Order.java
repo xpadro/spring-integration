a22f6cfbe94e17fa516d838bee5512e05e594a6f
Xavier Padro <Asus@Asus-PC>
2013-11-18 18:20:57 +0100

    First commit

---
+++ int-error/src/main/java/xpadro/spring/integration/model/Order.java
@@ -0,0 +1,33 @@
+package xpadro.spring.integration.model;
+
+import java.io.Serializable;
+
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String description;
+	
+	public Order() {}
+	
+	public Order(int id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ int-error/src/main/java/xpadro/spring/integration/service/OrderService.java
@@ -0,0 +1,8 @@
+package xpadro.spring.integration.service;
+
+import xpadro.spring.integration.model.Order;
+
+
+public interface OrderService {
+	public void sendOrder(Order order);
+}
