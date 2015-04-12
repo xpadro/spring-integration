a6699d581e4e595c656099d48f3842f11512c96a
xpadro <xpadro.sw@gmail.com>
2015-04-12 17:46:17 +0200

    Initial commit

---
+++ jms-boot-javaconfig/src/main/java/xpadro/spring/jms/JmsJavaconfigApplication.java
@@ -0,0 +1,12 @@
+package xpadro.spring.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsJavaconfigApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(JmsJavaconfigApplication.class, args);
+    }
+}
