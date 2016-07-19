479b2597f051437bf4a435c436c2ccafbb39ce0b
xpadro <xpadro.sw@gmail.com>
2016-07-19 14:48:50 +0200

    Added readme for File module

--- http/http-dsl/src/main/java/xpadro/spring/integration/server/configuration/InfrastructureConfiguration.java
+++ http/http-dsl/src/main/java/xpadro/spring/integration/server/configuration/InfrastructureConfiguration.java
@@ -23,84 +23,85 @@ import xpadro.spring.integration.server.model.ServerPerson;
 @EnableIntegration
 public class InfrastructureConfiguration {
 	
-	@Bean
-	public IntegrationFlow httpGetFlow() {
-		return IntegrationFlows.from(httpGetGate()).channel("httpGetChannel").handle("personEndpoint", "get").get();
-	}
-	
-	@Bean
-	public IntegrationFlow httpPostPutFlow() {
-		return IntegrationFlows.from(httpPostPutGate()).channel("routeRequest").route("headers.http_requestMethod", 
-				 m -> m.prefix("http").suffix("Channel")
-                 .channelMapping("PUT", "Put")
-                 .channelMapping("POST", "Post")
-		).get();
-	}
-	
-	@Bean
-	public IntegrationFlow httpPostFlow() {
-		return IntegrationFlows.from("httpPostChannel").handle("personEndpoint", "post").get();
-	}
-	
-	@Bean
-	public IntegrationFlow httpPutFlow() {
-		return IntegrationFlows.from("httpPutChannel").handle("personEndpoint", "put").get();
-	}
-	
-	@Bean
-	public IntegrationFlow httpDeleteFlow() {
-		return IntegrationFlows.from(httpDeleteGate()).channel("httpDeleteChannel").handle("personEndpoint", "delete").get();
-	}
-	
-	@Bean
-	public MessagingGatewaySupport httpGetGate() {
-		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
-		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.GET}, "/persons/{personId}"));
-		handler.setPayloadExpression(parser().parseExpression("#pathVariables.personId"));
-		handler.setHeaderMapper(headerMapper());
-
-		return handler;
-	}
-	
-	@Bean
-	public MessagingGatewaySupport httpPostPutGate() {
-		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
-		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.PUT, HttpMethod.POST}, "/persons", "/persons/{personId}"));
-		handler.setStatusCodeExpression(parser().parseExpression("T(org.springframework.http.HttpStatus).NO_CONTENT"));
-		handler.setRequestPayloadType(ServerPerson.class);
-		handler.setHeaderMapper(headerMapper());
-
-		return handler;
-	}
-	
-	@Bean
-	public MessagingGatewaySupport httpDeleteGate() {
-		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
-		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.DELETE}, "/persons/{personId}"));
-		handler.setStatusCodeExpression(parser().parseExpression("T(org.springframework.http.HttpStatus).NO_CONTENT"));
-		handler.setPayloadExpression(parser().parseExpression("#pathVariables.personId"));
-		handler.setHeaderMapper(headerMapper());
-
-		return handler;
-	}
-	
-	@Bean
-	public ExpressionParser parser() {
-		return new SpelExpressionParser();
-	}
-	
-	@Bean
-	public HeaderMapper<HttpHeaders> headerMapper() {
-		return new DefaultHttpHeaderMapper();
-	}
-	
-	private RequestMapping createMapping(HttpMethod[] method, String... path) {
-		RequestMapping requestMapping = new RequestMapping();
-		requestMapping.setMethods(method);
-		requestMapping.setConsumes("application/json");
-		requestMapping.setProduces("application/json");
-		requestMapping.setPathPatterns(path);
-		
-		return requestMapping;
-	}
+//	@Bean
+//	public IntegrationFlow httpGetFlow() {
+//		return IntegrationFlows.from(httpGetGate()).channel("httpGetChannel").handle("personEndpoint", "get").get();
+//	}
+//
+//	@Bean
+//	public IntegrationFlow httpPostPutFlow() {
+//		return null;
+//		return IntegrationFlows.from(httpPostPutGate()).channel("routeRequest").route("headers.http_requestMethod",
+//				 m -> m.prefix("http").suffix("Channel")
+//                 .channelMapping("PUT", "Put")
+//                 .channelMapping("POST", "Post")
+//		).get();
+//	}
+//
+//	@Bean
+//	public IntegrationFlow httpPostFlow() {
+//		return IntegrationFlows.from("httpPostChannel").handle("personEndpoint", "post").get();
+//	}
+//
+//	@Bean
+//	public IntegrationFlow httpPutFlow() {
+//		return IntegrationFlows.from("httpPutChannel").handle("personEndpoint", "put").get();
+//	}
+//
+//	@Bean
+//	public IntegrationFlow httpDeleteFlow() {
+//		return IntegrationFlows.from(httpDeleteGate()).channel("httpDeleteChannel").handle("personEndpoint", "delete").get();
+//	}
+//
+//	@Bean
+//	public MessagingGatewaySupport httpGetGate() {
+//		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
+//		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.GET}, "/persons/{personId}"));
+//		handler.setPayloadExpression(parser().parseExpression("#pathVariables.personId"));
+//		handler.setHeaderMapper(headerMapper());
+//
+//		return handler;
+//	}
+//
+//	@Bean
+//	public MessagingGatewaySupport httpPostPutGate() {
+//		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
+//		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.PUT, HttpMethod.POST}, "/persons", "/persons/{personId}"));
+//		handler.setStatusCodeExpression(parser().parseExpression("T(org.springframework.http.HttpStatus).NO_CONTENT"));
+//		handler.setRequestPayloadType(ServerPerson.class);
+//		handler.setHeaderMapper(headerMapper());
+//
+//		return handler;
+//	}
+//
+//	@Bean
+//	public MessagingGatewaySupport httpDeleteGate() {
+//		HttpRequestHandlingMessagingGateway handler = new HttpRequestHandlingMessagingGateway();
+//		handler.setRequestMapping(createMapping(new HttpMethod[]{HttpMethod.DELETE}, "/persons/{personId}"));
+//		handler.setStatusCodeExpression(parser().parseExpression("T(org.springframework.http.HttpStatus).NO_CONTENT"));
+//		handler.setPayloadExpression(parser().parseExpression("#pathVariables.personId"));
+//		handler.setHeaderMapper(headerMapper());
+//
+//		return handler;
+//	}
+//
+//	@Bean
+//	public ExpressionParser parser() {
+//		return new SpelExpressionParser();
+//	}
+//
+//	@Bean
+//	public HeaderMapper<HttpHeaders> headerMapper() {
+//		return new DefaultHttpHeaderMapper();
+//	}
+//
+//	private RequestMapping createMapping(HttpMethod[] method, String... path) {
+//		RequestMapping requestMapping = new RequestMapping();
+//		requestMapping.setMethods(method);
+//		requestMapping.setConsumes("application/json");
+//		requestMapping.setProduces("application/json");
+//		requestMapping.setPathPatterns(path);
+//
+//		return requestMapping;
+//	}
 }
